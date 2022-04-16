d0ca240cb59dc38ecb3b6400b9e30f969d024760
Shubham-Bhalekar <bshubham787916@gmail.com>
2022-04-16 14:51:40 +0530

    Redux initial

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -2,18 +2,22 @@ import "./App.css";
 import { Component } from "react";
 import Main from "./components/MainComponent";
 import { BrowserRouter } from 'react-router-dom';
+import { ConfigureStore } from "./redux/configureStore";
+import { Provider } from 'react-redux';
+
+const store = ConfigureStore();
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-  }
+
   render() {
     return (
+      <Provider store={store}>
       <BrowserRouter>
         <div>
           <Main />
         </div>
       </BrowserRouter>
+      </Provider>
     );
   }
 }
